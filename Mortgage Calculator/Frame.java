fa233ee3c4cf1e8b85dcc4bfb0bb619df83482b7
Taylor365 <carltaylor1988@hotmail.com>
2017-02-16 11:00:30 +0000

    Update Frame.java
    Completely re coded the UI and added in comments to make the application more user friendly. Also, cleaned up some error handling code.

--- Mortgage Calculator/Frame.java
+++ Mortgage Calculator/Frame.java
@@ -1,99 +1,104 @@
 package mortgageCalc;
 
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
-import java.text.NumberFormat;
+import java.awt.*;
+import java.awt.event.*;
+import java.text.*;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.*;
+
 
 public class Frame extends JFrame {
 	
-	public GridLayout layout;
+	//Create the layout to use.
+	GridBagLayout layout = new GridBagLayout();
+	GridBagConstraints gbc = new GridBagConstraints();
 	
-	public JLabel status;
-	public JLabel princeLabel;
-	public JLabel termLabel;
-	public JLabel intLabel;
-	public JLabel outputLabel;
-	public JLabel outputLabel2;
-	public JLabel outputLabel3;
-	public JLabel blankLabel;	
-	public JLabel blankLabel2;
-	
-	public JPanel menuButtonPanel;
-	public JPanel userPanel;
-	public JPanel userOutputPanel;
-	
-	public JTextField prince;
-	public JTextField term;
-	public JTextField interest;
-	
-	public JButton calc;
-	public JButton fileNew;
-	public JButton fileExit;
+	JTextField prince = new JTextField(10);
 	
 	public double total;
 	public double totalInt;
 	
 	public Frame(){
 		
-		menuButtonPanel = new JPanel();
-		userPanel = new JPanel();
+		gbc.anchor = GridBagConstraints.WEST; //Aligns objects to the left.
+		gbc.insets = new Insets(10, 10, 10, 10); //Inserts space between objects in each direction.
 		
-		layout = new GridLayout(0,2);
+		//Create panels to hold objects.
+		JPanel menuButtonPanel = new JPanel();
+		JPanel userPanel = new JPanel();
+		
+		//userPanel will use the GridBagLayout.
 		userPanel.setLayout(layout);
 		
-		princeLabel = new JLabel("Principle :");
-		termLabel = new JLabel("Term of Mortgage :");
-		intLabel = new JLabel("Interest Rate :");
-		outputLabel = new JLabel();
-		outputLabel2 = new JLabel();
-		outputLabel3 = new JLabel();
-		status = new JLabel("Welcome");
-		blankLabel = new JLabel("");
-		blankLabel2 = new JLabel("");
+		//Create the objects for the panel.
+		//Labels - used to print text to the frame.
+		JLabel princeLabel = new JLabel("Principle :");
+		JLabel termLabel = new JLabel("Term of Mortgage :");
+		JLabel intLabel = new JLabel("Interest Rate :");
+		JLabel outputLabel = new JLabel();
+		JLabel outputLabel2 = new JLabel();
+		JLabel outputLabel3 = new JLabel();
+		JLabel status = new JLabel("Welcome");
 		
-		fileNew = new JButton("New");
-		fileExit = new JButton("Exit");
-		calc = new JButton("Calculate");
+		//Buttons - used to invoke actions.
+		JButton fileNew = new JButton("New");
+		JButton fileExit = new JButton("Exit");
+		JButton calc = new JButton("Calculate");
 		
 		menuButtonPanel.add(fileNew);
 		menuButtonPanel.add(fileExit);
 	        
-        prince = new JTextField(10);
-        term = new JTextField(10);
-        interest = new JTextField(3);
+       		 //TextFields - used to accept user input or output text.
+        	JTextField term = new JTextField(10);
+        	JTextField interest = new JTextField(3);
         
-        userPanel.add(princeLabel);
-        userPanel.add(prince);
-        userPanel.add(termLabel);
-        userPanel.add(term);
-        userPanel.add(intLabel);
-        userPanel.add(interest);
-        userPanel.add(calc);  
-        userPanel.add(outputLabel);
-        userPanel.add(blankLabel);
-        userPanel.add(outputLabel2);
-        userPanel.add(blankLabel2);
-        userPanel.add(outputLabel3);
+        	//Plot out the positions for each object and add them to the User Panel.
+		gbc.gridx = 0; //Horizontal position
+		gbc.gridy = 0; //Vertical position
+        	userPanel.add(princeLabel, gbc);
+        	gbc.gridx = 1;
+		gbc.gridy = 0;
+        	userPanel.add(prince, gbc);
+        	gbc.gridx = 0;
+		gbc.gridy = 1;
+        	userPanel.add(termLabel, gbc);
+        	gbc.gridx = 1;
+		gbc.gridy = 1;
+        	userPanel.add(term, gbc);
+        	gbc.gridx = 0;
+		gbc.gridy = 2;
+        	userPanel.add(intLabel, gbc);
+        	gbc.gridx = 1;
+		gbc.gridy = 2;
+        	userPanel.add(interest, gbc);
+        	gbc.gridx = 0;
+		gbc.gridy = 3;
+        	userPanel.add(calc, gbc);
+        	gbc.gridx = 0;
+		gbc.gridy = 4;
+        	userPanel.add(outputLabel, gbc);
+        	gbc.gridx = 0;
+		gbc.gridy = 5;
+        	userPanel.add(outputLabel2, gbc);
+        	gbc.gridx = 0;
+		gbc.gridy = 6;
+        	userPanel.add(outputLabel3, gbc);
         
-        add(menuButtonPanel,BorderLayout.NORTH);
-        add(userPanel,BorderLayout.CENTER);
-		add(status, BorderLayout.SOUTH);
-			
+        	//Align each panel on the frame using BorderLayout.
+        	add(menuButtonPanel,BorderLayout.NORTH);
+        	add(userPanel,BorderLayout.CENTER);
+		add(status, BorderLayout.SOUTH); //This will display the status after each new action.
+		
+		//Create ActionListener for each button.
+		
+		//This will reset everything for a new calculation.
 		fileNew.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				if(JOptionPane.showConfirmDialog(null, "Are you sure you want to create a new calculation?") == 0){
 					status.setText("Generated New Calculation");
+					princeLabel.setText("Principle :");
+					termLabel.setText("Term of Mortgage :");
+					intLabel.setText("Interest Rate :");
 					prince.setText("");
 					term.setText("");
 					interest.setText("");
@@ -102,23 +107,25 @@ public class Frame extends JFrame {
 					outputLabel3.setText(null);
 				}
 			}
-		});
-		
+		});		
+		//This will close the application.
 		fileExit.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				if(JOptionPane.showConfirmDialog(null, "Are you sure you wish to exit?") == 0){
 					closeWindow();
 				}
 			}
-		});	
-		
+		});			
+		//This will run the calculation using the user's input.
 		calc.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				String princeText = prince.getText();
 		        String termText = term.getText();
 		        String interText = interest.getText();
 		        
-		        if(!princeText.matches("[0-9]+") && !princeText.matches("[0-9]+") && !termText.matches("[0-9]+")){
+	        	//Check if the user entered the correct numeric input.	
+		        //First we check all inputs, then cycle through each separately checking for numeric input.
+		        if(!princeText.matches("[0-9]+") && !termText.matches("[0-9]+") && !interText.matches("[0-9]+")){
 					outputLabel.setText("* Please enter the principle of the mortgage in numerics.");
 					outputLabel2.setText("* Please enter the term of the loan in years.");
 					outputLabel3.setText("* Please enter the interest rate as a whole number or decimals (e.g. 4.5 = 4.5%).");
@@ -126,42 +133,56 @@ public class Frame extends JFrame {
 					princeLabel.setText("Principle :	*");
 					termLabel.setText("Term of Mortgage :	*");
 					intLabel.setText("Interest Rate :	*");
+					status.setText("Error in Calculation");
 		        }else if(!princeText.matches("[0-9]+")){
 					outputLabel.setText("* Please enter the principle of the mortgage in numerics.");
 					princeLabel.setText("Principle :	*");
+					status.setText("Error in Calculation");
+		        }else if(!princeText.matches("[0-9]+")){
+					outputLabel.setText("* Please enter the principle of the mortgage in numerics.");
+					princeLabel.setText("Principle :	*");
+					status.setText("Error in Calculation");
 				}else if(!termText.matches("[0-9]+")){
 					princeLabel.setText("Principle :");
 					outputLabel.setText(null);
-					
 					outputLabel2.setText("* Please enter the term of the loan in years.");
 					termLabel.setText("Term of Mortgage :	*");
+					status.setText("Error in Calculation");
 				}else if(!interText.matches("[0-9]+.[0-9]+") && !interText.matches("[0-9]+")){
 					termLabel.setText("Term of Mortgage :");
+					outputLabel.setText(null);
 					outputLabel2.setText(null);
-					
 					outputLabel3.setText("* Please enter the interest rate as a whole number or decimals (e.g. 4.5 = 4.5%).");
 					intLabel.setText("Interest Rate :	*");
-				
+					status.setText("Error in Calculation");
 				}else{
+					outputLabel.setText(null);
+					outputLabel2.setText(null);
+					outputLabel3.setText(null);
+					
+					//Convert JTextField to Integer/Double and insert into the Calculation method.
 					int princeInt = Integer.parseInt(princeText);
 					int termInt = Integer.parseInt(termText);
-					double interInt = Double.parseDouble(interText);
+					double interDoub = Double.parseDouble(interText);
 					intLabel.setText("Interest Rate :");
 					
-					interestCalculation(princeInt, termInt, interInt);
-					outputLabel.setText("<html>Your monthly repayments will be: " + "€" + NumberFormat.getNumberInstance().format(total));
+					interestCalculation(princeInt, termInt, interDoub);
+					//Convert the output into number format(add in commas) and print to the User panel.
+					outputLabel.setText("Your monthly repayments will be: " + "€" + NumberFormat.getNumberInstance().format(total));
 					outputLabel2.setText("Your total interest paid for the loan will be: " + "€" + NumberFormat.getNumberInstance().format(totalInt));
 					outputLabel3.setText("Total paid back to the bank: " + "€" + NumberFormat.getNumberInstance().format((totalInt + princeInt)));
+					
+					status.setText("Calculation Complete");
 				}
 			}
 		});	
 	}
-		
+	//The close window method.	
 	public void closeWindow(){
 		WindowEvent close = new WindowEvent(this, WindowEvent.WINDOW_CLOSING);
 		Toolkit.getDefaultToolkit().getSystemEventQueue().postEvent(close);
-	}
-	
+	}	
+	//The Calculation method.
 	public void interestCalculation(int cost, int length, double rate){
 		rate = (rate/100)/12;
 		double powercalc = 	(1 - (Math.pow((1 + rate), (-length * 12))));	
@@ -169,19 +190,10 @@ public class Frame extends JFrame {
 		total = (rate * cost) / powercalc;
 		total = total * 100;
 		total = Math.round(total);
-		total = total / 100;
-		
+		total = total / 100;		
 		totalInt = (total * (length * 12) - cost);
 		totalInt = totalInt * 100;
 		totalInt = Math.round(totalInt);
-		totalInt = totalInt / 100;
-		
-		/*FOR TESTING
-		System.out.println("This is the rate after calculation " + rate);
-		System.out.println("This is the term after calculation " + length);
-		System.out.println("This is the principle after calculation " + cost);
-		System.out.println("This is the power calculation after calculation " + powercalc);
-		System.out.println("Your monthly repayments will be: " + total);
-		*/
+		totalInt = totalInt / 100;		
 	}
 }
